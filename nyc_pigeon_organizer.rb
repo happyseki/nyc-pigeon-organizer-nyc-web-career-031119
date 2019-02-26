95a0db14d9bf442516fc409c8bb3e9645ad83464
Elaina <elainaseki@gmail.com>
2019-02-26 21:33:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
+   pigeon_list = Hash.new
+  data.each do|attribute, value_hash|
+    value_hash.each do|key, name_array|
+    name_array.each do|name|
+    if  !pigeon_list.keys.include?(name)
+    pigeon_list[name]={}
+    end
+    if !pigeon_list[name].keys.include?(attribute)
+    pigeon_list[name][attribute] = []
+    end
+    if ! pigeon_list[name][attribute].include?(key)
+    pigeon_list[name][attribute] << key.to_s
+    end 
+    end
+   end
+  end 
+ pigeon_list
   # write your code here!
 end
